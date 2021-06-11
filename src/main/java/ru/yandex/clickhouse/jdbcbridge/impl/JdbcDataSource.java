925fd445c9bdaa462963d8808dd001a19f762c7d
Zhichun Wu <zhicwu@gmail.com>
2021-06-11 08:26:49 +0800

    Start connection pool without validating connection

--- src/main/java/ru/yandex/clickhouse/jdbcbridge/impl/JdbcDataSource.java
+++ src/main/java/ru/yandex/clickhouse/jdbcbridge/impl/JdbcDataSource.java
@@ -70,6 +70,7 @@ public class JdbcDataSource extends NamedDataSource {
     private static final Properties DEFAULT_DATASOURCE_PROPERTIES = new Properties();
 
     private static final String PROP_DRIVER_CLASS = "driverClassName";
+    private static final String PROP_INITIALIZATION_FAIL_TIMEOUT = "initializationFailTimeout";
     private static final String PROP_POOL_NAME = "poolName";
     private static final String PROP_PASSWORD = "password";
 
@@ -405,6 +406,9 @@ public class JdbcDataSource extends NamedDataSource {
                 }
             }
 
+            if (!props.containsKey(PROP_INITIALIZATION_FAIL_TIMEOUT)) {
+                props.setProperty(PROP_INITIALIZATION_FAIL_TIMEOUT, "0");
+            }
             props.setProperty(PROP_POOL_NAME, id);
 
             this.jdbcUrl = null;
